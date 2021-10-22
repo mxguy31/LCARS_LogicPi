aec7bf7679fdf0d572b3a753e931c5304587f940
Chris <57292883+mxguy31@users.noreply.github.com>
2021-10-21 20:47:29 -0400

    Add Kivy sql / text logging handlers

--- Start_GUI.py
+++ Start_GUI.py
@@ -3,8 +3,27 @@ from kivy import Config
 Config.set('graphics', 'width', '1280')
 Config.set('graphics', 'height', '800')
 from gui.lcars import LCARSApp
+from app.constants import CONST
+
+from logging import FileHandler
+from logging.handlers import RotatingFileHandler
+from app.syslog import mySQLHandler
+from kivy.logger import Logger
 
 if __name__ == '__main__':
+
+    if not CONST.LOG_ROTATE:
+        file_handler = FileHandler(CONST.LOG_FILE)
+    else:
+        file_handler = RotatingFileHandler(CONST.LOG_FILE,
+                                            'a',
+                                            CONST.LOG_MAX_SIZE,
+                                            CONST.LOG_BACKUPS)
+    file_handler.setFormatter(CONST.LOG_FORMAT)
+    sql_handler = mySQLHandler()
+    Logger.addHandler(file_handler)
+    Logger.addHandler(sql_handler)
+
     gui = LCARSApp()
     signal.signal(signal.SIGTERM, gui.stop)
     gui.run()
